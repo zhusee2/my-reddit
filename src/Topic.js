1ace7f7fd6188087ca07f2a53e8b1864f9c3ad3b
Teng-hao Chang <zhusee2@gmail.com>
2017-10-04 23:43:23 +0800

    Adjust styles to make <Topic> looks like Reddit

--- src/Topic.js
+++ src/Topic.js
@@ -1,21 +1,32 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
 
+import './styles/Topic.css';
+
 function Topic({ topic, onUpvote, onDownvote }) {
   return (
     <li className="topic-record">
+        <div className="topic-record-body">
+        {topic.content}
+      </div>
       <div className="topic-record-votes">
         {topic.votes}
       </div>
-      <div className="topic-record-body">
-        {topic.content}
-      </div>
       <div className="topic-record-actions">
-        <button type="button" onClick={onUpvote}>
-          ^ upvote
+        <button
+          type="button"
+          className="btn-upvote"
+          title="Upvote"
+          onClick={onUpvote}>
+          +
         </button>
-        <button type="button" onClick={onDownvote}>
-          v downvote
+        <br />
+        <button
+          type="button"
+          className="btn-downvote"
+          title="Downvote"
+          onClick={onDownvote}>
+          -
         </button>
       </div>
     </li>
