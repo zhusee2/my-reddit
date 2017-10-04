4ec4202f3ddf7a7120f33e8af06a0a0876bf53ed
Teng-hao Chang <zhusee2@gmail.com>
2017-10-04 15:59:06 +0800

    Setup a Redux store (with devtool) to app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
+import App from './App';
+
+import reducer from './reducer';
+import registerServiceWorker from './registerServiceWorker';
+import './index.css';
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root'));
+
 registerServiceWorker();
