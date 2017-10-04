774b2a8409a7c72852f2f31e7ee5988bf84b3904
Teng-hao Chang <zhusee2@gmail.com>
2017-10-04 17:41:25 +0800

    Sort and list the top 20 topics in <TopicsList>

--- src/TopicsList.js
+++ src/TopicsList.js
@@ -1,10 +1,11 @@
 import * as React from 'react';
 import * as PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 
 function TopicsList({ topics }) {
   return(
     <ul className="topics-list">
-      <li>Topic</li>
+      {topics.map(topic => <li key={topic.id}>{topic.content}</li>)}
     </ul>
   );
 }
@@ -13,4 +14,26 @@ TopicsList.propTypes = {
   topics: PropTypes.array,
 };
 
-export default TopicsList;
+function mapStateToProps(state) {
+  // Clone the array first to prevent contamination.
+  const allTopics = state.topics.slice();
+  const sortedTop20Topics = allTopics
+    .sort((topicA, topicB) => {
+      const votesDiff = topicA.votes - topicB.votes;
+
+      // If the share the same votes, prefer the recent-posted one.
+      if (votesDiff === 0) {
+        return topicB.id - topicA.id;
+      }
+
+      // Otherwise promote the one with higher votes.
+      return votesDiff;
+    })
+    .slice(0, 20);
+
+  return {
+    topics: sortedTop20Topics,
+  };
+}
+
+export default connect(mapStateToProps)(TopicsList);
