c21e7d4fe0c2f90092cbff7481f5ff44061135b1
Teng-hao Chang <zhusee2@gmail.com>
2017-10-04 17:28:10 +0800

    Fix the smoke test for <App>

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+
 import App from '../App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
+  const mockedStore = createStore(state => state);
+
+  ReactDOM.render(
+    <Provider store={mockedStore}>
+      <App />
+    </Provider>,
+  div);
 });
