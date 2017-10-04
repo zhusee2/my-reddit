95333a9d217fc94cd786a46ec25851f2b610b55d
Teng-hao Chang <zhusee2@gmail.com>
2017-10-04 17:31:15 +0800

    Add tests for reducer on topic creation

--- src/__tests__/reducer.test.js
+++ src/__tests__/reducer.test.js
@@ -1,9 +1,25 @@
 import reducer, { INITIAL_STATE } from '../reducer';
 
+import {
+  createTopic,
+} from '../actions';
+
 it('returns initial state on init action with no state given', () => {
   const action = {
     type: '@@INIT',
   };
 
   expect(reducer(undefined, action)).toEqual(INITIAL_STATE);
+});
+
+it('creates a topic record and adds to topics array on create action', () => {
+  const action = createTopic('foo-bar');
+  const nextState = reducer(undefined, action);
+
+  expect(nextState.topics).toHaveLength(1);
+  expect(nextState.topics[0]).toEqual({
+    id: 0,
+    content: 'foo-bar',
+    votes: 0,
+  });
 });
