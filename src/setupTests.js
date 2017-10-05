c89d61a7350c4f97482fe72403c79e1dd68029bd
Teng-hao Chang <zhusee2@gmail.com>
2017-10-05 21:05:01 +0800

    Fix React v16 keep warning about requestAnimationFrame in tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,4 +1,9 @@
-import Enzyme from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// Because JSDOM does not ship with this while React v16 requires it.
+global.requestAnimationFrame = function(callback) {
+  setTimeout(callback, 0);
+};
+
+const Enzyme = require('enzyme');
+const Adapter = require('enzyme-adapter-react-16');
 
 Enzyme.configure({ adapter: new Adapter() });
